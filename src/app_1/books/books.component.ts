3efbf939c259f4fc4fb90cbe11d02f956b9de430
supesiku <supesiku@gmail.com>
2023-02-16 17:06:49 +0900

    4

--- src/app_1/books/books.component.ts
+++ src/app_1/books/books.component.ts
@@ -1,5 +1,6 @@
-import { Component } from "@angular/core"
+import { Component, OnInit } from "@angular/core"
 import { Book } from "../types/book"
+import { BooksService } from "./books.service"
 
 
 @Component({
@@ -7,19 +8,24 @@ import { Book } from "../types/book"
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css']
 })
-export class BooksComponent{
-  books:Book[] = [{name:"Clean Code",author:"Tom",price:1900},
-                  {name:"Dirty deal",author:"D'arby",price:260}]
+export class BooksComponent implements OnInit{
+  books:Book[] = []
   cart:Book[] = []
-
   sumPrice:number = 0
-
   f1:boolean=true
   f2:boolean=true
   f3:boolean=true
+
+  constructor(private booksService:BooksService){
+    this.books = this.booksService.getBooks()
+  }
+  ngOnInit(): void {
+    
+  }
 
   addToCart(e:Book){
     this.cart.push(e)
     this.sumPrice = this.cart.reduce((s,e)=>{return s + e.price}, 0)
   }
+
 }
--- src/app_1/books/books.service.ts
+++ src/app_1/books/books.service.ts
@@ -6,4 +6,11 @@ import { Injectable } from '@angular/core';
 export class BooksService {
 
   constructor() { }
+
+
+
+  getBooks(){
+    return [{name:"Clean Code",author:"Tom",price:1900},
+                  {name:"Dirty deal",author:"D'arby",price:260}]
+  }
 }
