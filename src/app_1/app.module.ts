c4b9033f780fac02045b241268b3e64a96586bc7
supesiku <supesiku@gmail.com>
2023-02-17 15:15:29 +0900

    added bookmodule

--- src/app_1/app.module.ts
+++ src/app_1/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { BooksComponent } from "src/app_1/books/books.component";
-import { BookComponent } from "src/app_1/book/book.component";
 import { AppComponent } from "src/app_1/app.component";
 import { FormsModule } from "@angular/forms";
 import { MsgService } from "src/app_1/books/msg.service"
 import { MsgServiceJp } from "src/app_1/books/msg.service.jp"
+import { BooksModule } from "./books/books.module";
+import { BooksComponent } from "./books/books.component";
 
 @NgModule(
   {
-    imports: [BrowserModule, FormsModule],
-    declarations: [AppComponent, BooksComponent, BookComponent],
+    imports: [BrowserModule, FormsModule, BooksModule],
+    declarations: [AppComponent],
     bootstrap: [AppComponent],
     // providers: [{provide:MsgService, useClass:MsgServiceJp}]
   }
